7da12e6d282b260a154c53b6b342810e47b15182
pasindu-codimite <pasindu.rupasinghe@codimite.com>
2024-01-27 03:29:24 +0530

    getScaleNumber cleaned

--- src/utils/dimentions/refDimentions.ts
+++ src/utils/dimentions/refDimentions.ts
@@ -1,7 +1,12 @@
-import {Dimensions, PixelRatio} from 'react-native';
+import {Dimensions} from 'react-native';
 
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('screen');
 
+/**
+ * This function is used to scale the size of the components
+ * @param size
+ * @returns
+ */
 const getScaleNumber = (size: number) => {
   const dimesion = Math.min(SCREEN_HEIGHT, SCREEN_WIDTH);
   const dpi = Math.round(dimesion / 375);
@@ -11,65 +16,4 @@ const getScaleNumber = (size: number) => {
   return size;
 };
 
-const widthBaseScale = SCREEN_WIDTH / 414;
-const heightBaseScale = SCREEN_HEIGHT / 896;
-
-function normalize(size: number, based = 'width') {
-  const newSize =
-    based === 'height' ? size * heightBaseScale : size * widthBaseScale;
-  return Math.round(PixelRatio.roundToNearestPixel(newSize));
-}
-
-const widthPixel = (size: number) => {
-  return normalize(size, 'width');
-};
-//for height  pixel
-const heightPixel = (size: number) => {
-  return normalize(size, 'height');
-};
-//for font  pixel
-const fontPixel = (size: number) => {
-  return heightPixel(size);
-};
-//for Margin and Padding vertical pixel
-const pixelSizeVertical = (size: number) => {
-  return heightPixel(size);
-};
-//for Margin and Padding horizontal pixel
-const pixelSizeHorizontal = (size: number) => {
-  return widthPixel(size);
-};
-//for Scene Builder flow layout
-const screenDisplayRatio = (width: number, height: number) => {
-  let r = 1;
-  const w = SCREEN_WIDTH < SCREEN_HEIGHT ? SCREEN_WIDTH : SCREEN_HEIGHT;
-  r = w / (height * 3.8);
-  if (width * r > w * 0.45) {
-    r = (w * 0.45) / width;
-  }
-  return r;
-};
-
-const panaromaDisplayClearence = (width: number, height: number) => {
-  let r = 1;
-  let d = 0;
-  const w = SCREEN_WIDTH < SCREEN_HEIGHT ? SCREEN_WIDTH : SCREEN_HEIGHT;
-  r = w / (height * 3.8);
-  if (width * r > w * 0.45) {
-    d = height * r - height * ((w * 0.45) / width);
-  }
-  return d;
-};
-
-export {
-  widthPixel,
-  heightPixel,
-  fontPixel,
-  pixelSizeVertical,
-  pixelSizeHorizontal,
-  getScaleNumber,
-  screenDisplayRatio,
-  panaromaDisplayClearence,
-  SCREEN_HEIGHT,
-  SCREEN_WIDTH,
-};
+export {getScaleNumber, SCREEN_HEIGHT, SCREEN_WIDTH};
